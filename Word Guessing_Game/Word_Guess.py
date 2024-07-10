e0e892b2285c1572eea4e7c3e7983c82a6eb6871
Atul Garg <39157649+atulgarg721@users.noreply.github.com>
2024-07-10 12:49:10 +0530

    Created word guessing game

---
+++ Word Guessing_Game/Word_Guess.py
@@ -0,0 +1,35 @@
+import random
+
+name = input("Enter your Player Name :")
+print(f"Welcome {name}. Best of Luck!")
+
+turns = 12
+words = ["Sam","Dictonary","rainbow","computer","science","english","mathematics","statistics","programming","water","board","geeks","for","reverse","player"]
+# one random word from words list
+word = random.choice(words)
+
+print(f"Guess the {len(word)} word Characters")
+guesses = ""
+print(word)
+while turns>0:
+    #no. of times user failed
+    failed = 0
+    for char in word:
+        if char in guesses :
+            print(char,end="")
+        else:
+            print("_")
+            failed += 1
+    
+    if failed == 0:
+        print("\nYou win")
+        print(f"The word is : {word}")
+        break
+    Guess = input("Guess a character :")
+    guesses += Guess
+    if Guess not in word:
+        turns -= 1
+        print("wrong")
+        print(f"You have {turns} more guesses left")
+        if turns ==0:
+            print("You lose")
