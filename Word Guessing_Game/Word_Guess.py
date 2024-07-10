c1ecba31d95b4839488809380365ab43cb2f8738
Atul Garg <39157649+atulgarg721@users.noreply.github.com>
2024-07-10 13:19:11 +0530

    Added Comments for better understanding of code

--- Word Guessing_Game/Word_Guess.py
+++ Word Guessing_Game/Word_Guess.py
@@ -1,3 +1,7 @@
+#Logic : you get a Guess character from user .if guess is not in the random word(word) a turn will be deducted and you will be shown remaining turns but if you have guessed the character correctly no turn deducted and you are asked for next character untill you guessed it all 
+
+
+
 import random
 
 name = input("Enter your Player Name :")
@@ -8,25 +12,28 @@ words = ["Sam","Dictonary","rainbow","computer","science","english","mathematics
 # one random word from words list
 word = random.choice(words)
 
-print(f"Guess the {len(word)} word Characters")
+print(f"Guess the {len(word)} Character word")
 guesses = ""
-print(word)
 while turns>0:
     #no. of times user failed
     failed = 0
+    #Prints your guesses
     for char in word:
         if char in guesses :
             print(char,end="")
         else:
             print("_")
             failed += 1
-    
+    # if all character of the word are in guesses then it means that  you have guessed the word
     if failed == 0:
         print("\nYou win")
         print(f"The word is : {word}")
         break
+    #Gets character from user
     Guess = input("Guess a character :")
+    # add that guess in guesses
     guesses += Guess
+    #main logic of checking character in word
     if Guess not in word:
         turns -= 1
         print("wrong")
